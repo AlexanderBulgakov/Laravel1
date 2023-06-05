b8719f1616ebf7f2954117440e9dddd3dba2fc2b
Alexander <alexander@localhost.loc>
2023-06-05 12:22:08 +0300

    added api route person

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/person', function () {
+    return response()->json(
+        [
+            'name' => 'test name',
+            'age' => '100',
+            'city' => 'City123'
+        ]
+    );
+});
