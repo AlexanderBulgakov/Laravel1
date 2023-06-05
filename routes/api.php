b3053dbbbbd933a31fb99bbbf73f76a1c4687d86
Alexander <alexander@localhost.loc>
2023-06-05 12:32:13 +0300

    added api route person/name

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,23 @@ Route::get('/person', function () {
         ]
     );
 });
+
+Route::get('/person/{name}', function (string $name) {
+    $example_names = [
+        'Test1',
+        'John'
+    ];
+
+    $key = array_search($name, $example_names);
+    if($key !== false){
+        $res = [
+            'name' => $example_names[ $key ]
+        ];
+    } else {
+        $res = [
+            'error' => 'Name not found'
+        ];
+    }
+
+    return response()->json($res);
+});
