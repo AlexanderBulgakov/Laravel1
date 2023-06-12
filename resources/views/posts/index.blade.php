5591672c2e52dfadff2a2c9e9d2e736408a8f486
Alexander <alexander@localhost.loc>
2023-06-12 10:57:12 +0300

    fixed index, added condition for posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,23 +3,27 @@
 @section('title', 'All posts')
 
 @section('content')
-<hr />
-<a style="color:green;" href="{{ route('posts.create') }}">Create</a>
-<hr />
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($posts as $post)
-            <tr>
-                <td>{{ $post->id }}</td>
-                <td><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+    <hr />
+    <a style="color:green;" href="{{ route('posts.create') }}">Create</a>
+    <hr />
+    @if($posts->isNotEmpty())
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($posts as $post)
+                    <tr>
+                        <td>{{ $post->id }}</td>
+                        <td><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>No posts available</p>
+    @endif
 @endsection
