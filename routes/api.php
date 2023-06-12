29070b818327ab5ae91ae382e176441da5dd7652
Alexander <alexander@localhost.loc>
2023-06-12 11:33:50 +0300

    fixed api route

--- routes/api.php
+++ routes/api.php
@@ -30,20 +30,24 @@ Route::get('/person', function () {
 
 Route::get('/person/{name}', function (string $name) {
     $example_names = [
-        'Test1',
+        'Test',
         'John'
     ];
 
     $key = array_search($name, $example_names);
-    if($key !== false){
-        $res = [
-            'name' => $example_names[ $key ]
-        ];
-    } else {
-        $res = [
-            'error' => 'Name not found'
-        ];
+    
+    if($key === false){
+        return response()->json(
+            [
+                'error' => 'Name not found'
+            ],
+            404
+        );
     }
 
-    return response()->json($res);
-});
+    return response()->json(
+        [
+            'name' => $example_names[$key]
+        ]
+    );
+})->whereAlpha('name');
